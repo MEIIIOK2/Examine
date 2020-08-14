72e868c511b319221f80eb5bc7a75bb10f9509be
MEIIIOK2 <mishasolomin344@gmail.com>
2020-08-14 13:18:24 +0300

    Управление + Границы экрана

---
+++ Assets/Scripts/BorderControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderControl : MonoBehaviour
+{
+    private static float border = 0;
+
+    public static float Border
+    {
+        get
+        {
+            if (border==0)
+            {
+                var cam = Camera.main;
+                border = cam.aspect * cam.orthographicSize;
+
+            }
+            return border;
+        }
+        private set { }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private Vector3 initialPosition;
+
+    [SerializeField] private int healthCount;
+
+    [SerializeField]private float playerSpeed;
+
+    [SerializeField] private Joystick joystick;
+
+    private Vector3 movevector = new Vector3();
+
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = initialPosition;
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; 
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y; 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+#if UNITY_ANDROID
+        movevector.x = joystick.Horizontal;
+        movevector.y = joystick.Vertical;
+#endif
+
+#if UNITY_EDITOR
+        movevector.x = Input.GetAxis("Horizontal");
+        movevector.y = Input.GetAxis("Vertical");
+#endif
+        transform.position += movevector * playerSpeed;
+        Vector3 clPos = transform.position;
+        clPos.x = Mathf.Clamp(clPos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
+        clPos.y = Mathf.Clamp(clPos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
+        transform.position = clPos;
+    }
+
+    
+}
